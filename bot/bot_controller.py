bcdf38d8f7c0a89359a8639058f0dc157836ce00
James Marshall Reber <jmarshallreber@gmail.com>
2017-08-16 22:03:24 -0400

    added monthly check

--- bot/bot_controller.py
+++ bot/bot_controller.py
@@ -8,7 +8,7 @@ class BotController:
   chores_weekly = ['(Living Room and Hall)', '(Bathroom)', '(Kitchen)']
   last_date = datetime.datetime.now().date()
   last_week = datetime.datetime.now().date()
-
+  check_monthly = False
   chore_assignment_daily = {'James' : '(Dishes)', 'Chase' : '(Trash)', 'Mike' : '(General Cleanliness)'}
   chore_assignment_weekly = {'James': '(Living Room and Hall)', 'Chase' : '(Bathroom)', 'Mike' : '(Kitchen)'}
 
@@ -56,15 +56,20 @@ class BotController:
 
   def process_message(self, recd_msg):
     msg_to_send = {}  # reply
+    msg_to_send['text'] = ""
     current_date = datetime.datetime.now().date()
+    if current_date.day == 28 and BotController.check_monthly == False:
+      msg_to_send['text'] += "Don't forget about rent! "
+      BotController.check_monthly = True
+    elif current_date.day != 28 and BotController.check_monthly == True:
+      BotController.check_monthly = False
     if int(abs(BotController.last_week - current_date).days) >= 7:
       BotController.update_weekly()
       BotController.update_daily()
-      msg_to_send['text'] = 'It\'s a new week! I\'ve updated the chores. Ask me about chores to see.'
-      return msg_to_send
+      msg_to_send['text'] += 'It\'s a new week! I\'ve updated the chores. Ask me about chores to see.'
     if BotController.last_date != current_date:
       BotController.update_daily()
-      msg_to_send['text'] = 'It\'s a new day! I\'ve updated the chores. Ask me about chores to see'
+      msg_to_send['text'] += 'It\'s a new day! I\'ve updated the chores. Ask me about chores to see'
       return msg_to_send
 
     # Preprocessing
@@ -76,15 +81,15 @@ class BotController:
     # Use some hard-coded rules to decide what this message says
     if used_any(BotController.GREETING_WORDS):
       if recd_msg['author'] == 'Grandpa' or recd_msg['author'] == 'James':
-        msg_to_send['text'] = 'Hello master.'
+        msg_to_send['text'] += 'Hello master.'
       msg_to_send['text'] = 'Greetings to you, as well, {}!'.format(recd_msg['author'])
     elif used_any(BotController.DATE_WORDS):
-      msg_to_send['text'] = 'The last date is ' + str(BotController.last_date) + ' and the current date is ' + str(current_date)
+      msg_to_send['text'] += 'The last date is ' + str(BotController.last_date) + ' and the current date is ' + str(current_date)
     elif used_any(BotController.HELP_WORDS):
-      msg_to_send['text'] = ('Hi! I\'m the friendly house mate, the chatbot.  I don\'t do much right now,' +
+      msg_to_send['text'] += ('Hi! I\'m the friendly house mate, the chatbot.  I don\'t do much right now,' +
                              ' but I will help remind you who has to do what chore.')
     elif used_any(BotController.CHORES_WORDS):
-      msg_to_send['text'] = ""
+      msg_to_send['text'] += ""
       for i in BotController.chore_assignment_daily:
         msg_to_send['text'] += i
         msg_to_send['text'] += ": your daily chore is "
@@ -94,10 +99,10 @@ class BotController:
         msg_to_send['text'] += " "
       msg_to_send['text'] += "-- make sure to do them!"
     elif used_any(BotController.JOKE_WORDS):
-      msg_to_send['text'] = random.choice(BotController.JOKES)
+      msg_to_send['text'] += random.choice(BotController.JOKES)
     elif used_any(BotController.SONG_WORDS):
-      msg_to_send['text'] = random.choice(BotController.SONG)
+      msg_to_send['text'] += random.choice(BotController.SONG)
     else:
-      msg_to_send['text'] = 'I can\'t tell what you\'re talking about.'
+      msg_to_send['text'] += 'I can\'t tell what you\'re talking about.'
 
     return msg_to_send
